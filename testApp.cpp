4934ce7881ac923b8705ce66aa005aeaff034d7f
BohdanKhomik <bodianych2020@gmail.com>
2021-10-01 16:59:29 +0300

    New_Resolved_Conflict

--- testApp.cpp
+++ testApp.cpp
@@ -4,15 +4,9 @@ using namespace std;
 
 int main()
 {
-<<<<<<< HEAD
-    cout << "Hello, world!" << endl;
     int a = 5;
     cout << "Your number is =" << a << endl;
-    system("pause");
-=======
     cout << "Hello, beautiful world! " << endl;
-    cout << "Hello, feveryone " << endl;
     cout << "BB everyone" << endl;
->>>>>>> conflictBranch
     return 0;
 }
